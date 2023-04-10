e29ed497c2dfea08f538716618bf5bc3d491fbc7
Deimos <deimoshall@gmail.com>
2023-04-10 17:27:09 -0600

    feat: add a refresh option

--- main.py
+++ main.py
@@ -12,11 +12,14 @@ while True:
         print(f"{i}: {port}")
 
     print("e: Exit")
+    print("r: Refresh")
 
     # Select serial port
     port_number = input('Select serial port: ')
     if port_number == 'e':
         break
+    elif port_number == 'r':
+        continue
 
     port = f"/dev/{ports[int(port_number)]}"
 
